25ec38d5ed76cb3a2abc264b233c52c7e9b017c1
Boset <boss-x86@protonmail.com>
2024-03-23 22:08:25 -0600

    login + logout

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,18 @@
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 from django.urls import path
 from .views import users
+from .views import auth
 
 urlpatterns = [
-   path('admin/', admin.site.urls),
-   path('register/', users.RegisterView.as_view()),
+    # Admin
+    path('admin/', admin.site.urls),
+    # Home Redirect
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
+    # Create User
+    path('register/', users.RegisterView.as_view()),
+    # Login
+    path('token/', auth.CustomAuthToken.as_view()),
+    # Logout
+    path('logout/', auth.Logout.as_view()),    
 ]
